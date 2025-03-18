27ddff3462723a05b38c1d940cb2401ebbab5ce4
Wang Rong <wangron@tesla.com>
2025-03-18 11:18:35 +0800

    [feat] add m2m auth

--- http/client/auth/m2m_impl.go
+++ http/client/auth/m2m_impl.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -27,6 +28,18 @@ type m2mAuthSet struct {
 	clientSecret string
 	resource     string
 	scope        string
+}
+
+type M2MRequestBody struct {
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+	Resource     string `json:"resource"`
+	Scope        string `json:"scope"`
+}
+type M2MResponseBody struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
 }
 
 func NewM2mAuthSet(cfg cloudroot.AuthSetConfig) (IAuthSet, error) {
@@ -55,7 +68,42 @@ func (m *m2mAuthSet) SetRequest(req *http.Request) error {
 	token, err := m.cache.GetString(m.cacheKey)
 	if err == nil && token != "" {
 		req.Header.Add(authHeader, prefix+token)
+		return nil
 	}
+
+	m.mux.Lock()
+	defer m.mux.Unlock()
+
+	token, err = m.cache.GetString(m.cacheKey)
+	if err == nil && token != "" {
+		req.Header.Add(authHeader, prefix+token)
+	}
+	//
+	resp, err := m.client.Post(req.Context(), "", &requests.HTTPOptions{
+		Body: M2MRequestBody{
+			ClientId:     m.clientId,
+			ClientSecret: m.clientSecret,
+			Resource:     m.resource,
+			Scope:        m.scope,
+		},
+	})
+	if err != nil {
+		return err
+	}
+	if resp == nil {
+		return fmt.Errorf("empty response when get m2m token")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid status code: %d", resp.StatusCode)
+	}
+	var res M2MResponseBody
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return err
+	}
+	duration := time.Duration(res.ExpiresIn-300) * time.Second
+	m.cache.Set(m.cacheKey, res.AccessToken, duration)
+	req.Header.Add(authHeader, prefix+token)
 	return nil
 }
 
